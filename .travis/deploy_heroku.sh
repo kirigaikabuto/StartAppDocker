15ff7d5c8adcf71e2f1c11ca722df6b8854e0025
Yerassyl <tleugazy98@gmail.com>
2021-07-18 14:19:00 +0600

    Sdsddsd

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-container-registry
+heroku container:login
 docker login -u $DOCKER_USER -p $DOCKER_PASS --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web
+heroku container:release web --app $HEROKU_APP_NAME
