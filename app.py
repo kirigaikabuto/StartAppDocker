660ab3bce789e17c9c20e23cc6e4180fafb93da6
Yerassyl <tleugazy98@gmail.com>
2021-07-18 12:57:26 +0600

    sdsdsdsd

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
+
 @app.route("/")
 def generate_buzz():
     page = '<html><body><h1>'
@@ -14,5 +15,6 @@ def generate_buzz():
     page += '</h1></body></html>'
     return page
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.getenv('PORT')) # port 5000 is the default
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))  # port 5000 is the default
